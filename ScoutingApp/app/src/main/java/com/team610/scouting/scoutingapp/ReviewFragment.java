5f29613366c921e1a41bbebace4cbfcb69fc71e6
Jack Williamson <jackwilliamson@crescentschool.org>
2016-03-01 23:32:54 -0500

    finished review page, may need to be formatted for smaller screens

--- ScoutingApp/app/src/main/java/com/team610/scouting/scoutingapp/ReviewFragment.java
+++ ScoutingApp/app/src/main/java/com/team610/scouting/scoutingapp/ReviewFragment.java
@@ -36,6 +36,27 @@ public class ReviewFragment extends Fragment {
     TextView teamNum;
     TextView[] defences = new TextView[4];
     TextView autonCrossedDefence;
+    TextView reachedDefence;
+    TextView spybot;
+    TextView autonBall;
+    TextView autonRobotEnd;
+    TextView[] teleopDefences = new TextView[5];
+    TextView highGoal;
+    TextView lowGoal;
+    TextView courtyard;
+    TextView fouls;
+    TextView hang;
+    TextView challenge;
+    TextView breach;
+    TextView capture;
+    TextView checkmate;
+    TextView defenceShots;
+    TextView popShots;
+    TextView defensiveRating;
+    TextView scoutName;
+    TextView comment;
+
+
     MatchData match;
 
     Button submitButton;
@@ -100,8 +121,124 @@ public class ReviewFragment extends Fragment {
         teamNum = (TextView) v.findViewById(R.id.team_num_review_TextView);
         teamNum.setText("Team: " + match.team);
 
+        defences[0] = (TextView) v.findViewById(R.id.defence1_selection_TextView);
+        defences[0].setText("1. " + match.defenceTypes[0]);
+
+        defences[1] = (TextView) v.findViewById(R.id.defence2_selection_TextView);
+        defences[1].setText("2. " + match.defenceTypes[1]);
+
+        defences[2] = (TextView) v.findViewById(R.id.defence3_selection_TextView);
+        defences[2].setText("3. " + match.defenceTypes[2]);
+
+        defences[3] = (TextView) v.findViewById(R.id.defence4_selection_TextView);
+        defences[3].setText("4. " + match.defenceTypes[3]);
+
         autonCrossedDefence = (TextView) v.findViewById(R.id.auton_crossed_defence_TextView);
         autonCrossedDefence.setText("Defence Crossed: " + match.crossedDefence);
+
+        reachedDefence = (TextView) v.findViewById(R.id.reached_defence_TextView);
+        reachedDefence.setText("Reached Defence: " + match.reachDefence);
+
+        spybot = (TextView) v.findViewById(R.id.spybot_TextView);
+        spybot.setText("Spybot: " + match.spybot);
+
+        String ballLocation = "";
+        if(match.scoredHighGoal){
+            ballLocation = "High Goal";
+        }
+        if(match.scoredLowGoal){
+            if(!ballLocation.equals("")){
+                ballLocation = "error";
+            }
+            else{
+                ballLocation = "Low Goal";
+            }
+        }
+        if(match.placedCourtyard){
+            if(!ballLocation.equals("")){
+                ballLocation = "error";
+            }
+            else{
+                ballLocation = "Courtyard Drop";
+            }
+        }
+
+        autonBall = (TextView) v.findViewById(R.id.auton_ball_TextView);
+        autonBall.setText("Ball: " + ballLocation);
+
+
+        String robotLocation = "";
+        if(match.endedCourtyard){
+            robotLocation = "Courtyard";
+        }
+        if(match.endedNeutralZone){
+            if(!robotLocation.equals("")){
+                robotLocation = "error";
+            }
+            else{
+                robotLocation = "Neutral Zone";
+            }
+        }
+        autonRobotEnd = (TextView) v.findViewById(R.id.auton_robot_location_TextView);
+        autonRobotEnd.setText("Robot Ended: " + robotLocation);
+
+
+        teleopDefences[0] = (TextView) v.findViewById(R.id.teleop_defence1_TextView);
+        teleopDefences[0].setText("1. " + match.defence1Cross + "crosses, " + chooseDefenceRating(match.defence1Rating));
+
+        teleopDefences[1] = (TextView) v.findViewById(R.id.teleop_defence2_TextView);
+        teleopDefences[1].setText("2. " + match.defence2Cross+"crosses, " + chooseDefenceRating(match.defence2Rating));
+
+        teleopDefences[2] = (TextView) v.findViewById(R.id.teleop_defence3_TextView);
+        teleopDefences[2].setText("3. " + match.defence3Cross+"crosses, " + chooseDefenceRating(match.defence3Rating));
+
+        teleopDefences[3] = (TextView) v.findViewById(R.id.teleop_defence4_TextView);
+        teleopDefences[3].setText("4. " + match.defence4Cross+"crosses, " + chooseDefenceRating(match.defence4Rating));
+
+        teleopDefences[4] = (TextView) v.findViewById(R.id.teleop_defence5_TextView);
+        teleopDefences[4].setText("5. " + match.defence5Cross+"crosses, " + chooseDefenceRating(match.defence5Rating));
+
+        highGoal = (TextView) v.findViewById(R.id.high_goal_teleop_TextView);
+        highGoal.setText("HighGoal - Scores: " + match.highGoalScores+", Misses: " + match.highGoalMisses);
+
+        lowGoal = (TextView) v.findViewById(R.id.low_goal_teleop_TextView);
+        lowGoal.setText("LowGoal - Scores: " + match.lowGoalScores+", Misses: " + match.lowGoalMisses);
+
+        courtyard = (TextView) v.findViewById(R.id.courtyard_drop_teleop_TextView);
+        courtyard.setText("Courtyard - Scores: " + match.courtyardScores+", Misses: " + match.courtyardMisses);
+
+        fouls = (TextView) v.findViewById(R.id.foul_TextView);
+        fouls.setText("Fouls: " +match.fouls);
+
+        hang = (TextView) v.findViewById(R.id.hang_TextView);
+        hang.setText("Hang: " + match.hang);
+
+        challenge = (TextView) v.findViewById(R.id.challenge_TextView);
+        challenge.setText("Challenge: " + match.challenge);
+
+        breach = (TextView) v.findViewById(R.id.breach_TextView);
+        breach.setText("Breach: " + match.breach);
+
+        capture = (TextView) v.findViewById(R.id.capture_TextView);
+        capture.setText("Capture: " + match.capture);
+
+        checkmate = (TextView) v.findViewById(R.id.checkmate_TextView);
+        checkmate.setText("Checkmate Shots: " + match.shotFromCheckMate);
+
+        defenceShots = (TextView) v.findViewById(R.id.defence_shots_TextView);
+        defenceShots.setText("Defence Shots: " +  match.shotFromDefences);
+
+        popShots = (TextView) v.findViewById(R.id.pop_shots_TextView);
+        popShots.setText("Pop Shots: " + match.shotFromPopShot);
+
+        defensiveRating = (TextView) v.findViewById(R.id.defence_rating_TextView);
+        defensiveRating.setText("Defensive Rating: " + match.defensiveRating);
+
+        scoutName = (TextView) v.findViewById(R.id.scout_name_TextView);
+        scoutName.setText("Scout: " + match.scoutName);
+
+        comment = (TextView) v.findViewById(R.id.comment_TextView);
+        comment.setText(match.comment);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
 
@@ -126,6 +263,26 @@ public class ReviewFragment extends Fragment {
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onReviewFragmentInteraction(uri);
+        }
+    }
+
+    public static String chooseDefenceRating(int rate){
+        switch (rate){
+            case 0:
+                return "white";
+                //break;
+            case 1:
+                return "green";
+                //break;
+            case 2:
+                return "yellow";
+                //break;
+            case 3:
+                return "red";
+                //break;
+            default:
+                return "error";
+                //break;
         }
     }
 
